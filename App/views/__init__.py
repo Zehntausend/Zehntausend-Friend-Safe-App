7c7d810988abdf400f583c9fa77132d651425227
Shihigh <64621373+Shihigh@users.noreply.github.com>
2023-04-24 00:29:22 +0000

    added templates to octaktemp2

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -1,7 +1,7 @@
 # blue prints are imported 
 # explicitly instead of using *
 from .user import user_views
-from .index import index_views
+#from .index import index_views
 from .auth import auth_views
 
 
